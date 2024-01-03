6ee4444b9a8221ca943b025f8153c05eb0db7065
Praveen <114330683+Leokinguu@users.noreply.github.com>
2024-01-03 18:21:25 +0530

    Change Binance Future to Spot

--- bin.py
+++ bin.py
@@ -7,7 +7,7 @@ import logging
 from forex_service import *
 import datetime
 
-endpoint = 'https://fapi.binance.com/fapi/v1/'
+endpoint = 'https://api.binance.com/api/v3/'
 logging.basicConfig(level=logging.INFO)
 
 def make_signed_request( method, endpoint, params=None):
@@ -104,7 +104,7 @@ async def place_orders_crypto(symbol, price1, price2, tp1, tp2, sl):
         # current_price_float = float(current_price)
 
         if current_price is not None and price1 <= current_price <= price2:
-            quantity = round(25 / current_price, 1)
+            quantity = round(10 / current_price, 1)
             params = {
                 'symbol': symbol,
                 'side': 'BUY',
@@ -124,7 +124,7 @@ async def place_orders_crypto(symbol, price1, price2, tp1, tp2, sl):
                 'symbol': symbol,
                 'side': 'SELL',
                 'type': 'TAKE_PROFIT_MARKET',
-                'quantity': round(25 / current_price)/2,
+                'quantity': round(10 / current_price)/2,
                 'stopPrice': tp1
             }
 
@@ -132,36 +132,36 @@ async def place_orders_crypto(symbol, price1, price2, tp1, tp2, sl):
                 'symbol': symbol,
                 'side': 'SELL',
                 'type': 'TAKE_PROFIT_MARKET',
-                'quantity': round(25 / current_price)/2,
+                'quantity': round(10 / current_price)/2,
                 'stopPrice': tp2
             }
 
-            leverage = set_leverage(symbol)
-            if leverage is not None:
-                print('lev', leverage)
-                position = set_margin_type(symbol)
-                print('po1', position)
-                if position is not None:
-                    print('po', position)
-                    orderData = make_signed_request('POST', f'{endpoint}order', params=params)
-                    if orderData is not None:
-                        orderDataSl = make_signed_request('POST', f'{endpoint}order', params=params_sl)
-                        orderDataTp1 = make_signed_request('POST', f'{endpoint}order', params=params_tp1)
-                        orderDataTp2 = make_signed_request('POST', f'{endpoint}order', params=params_tp2)
-                        log_data = {
-                            "result": {
-                                "trade": orderData,
-                                "stopLoss": orderDataSl,
-                                "takeProfit1": orderDataTp1,
-                                "takeProfit2": orderDataTp2
-                            }
-                        }
-                        logging.info(f'Trade deatils: {str(log_data)}')
-                        trade={}
-                        current_datetime = datetime.datetime.now()
-                        trade['trade'] = log_data
-                        trade['date'] = current_datetime
-                        crypto_trade_details(trade)
+            # leverage = set_leverage(symbol)
+            # if leverage is not None:
+            #     print('lev', leverage)
+            #     position = set_margin_type(symbol)
+            #     print('po1', position)
+            #     if position is not None:
+            #         print('po', position)
+            orderData = make_signed_request('POST', f'{endpoint}order', params=params)
+            if orderData is not None:
+                orderDataSl = make_signed_request('POST', f'{endpoint}order', params=params_sl)
+                orderDataTp1 = make_signed_request('POST', f'{endpoint}order', params=params_tp1)
+                orderDataTp2 = make_signed_request('POST', f'{endpoint}order', params=params_tp2)
+                log_data = {
+                    "result": {
+                        "trade": orderData,
+                        "stopLoss": orderDataSl,
+                        "takeProfit1": orderDataTp1,
+                        "takeProfit2": orderDataTp2
+                    }
+                }
+                logging.info(f'Trade deatils: {str(log_data)}')
+                trade={}
+                current_datetime = datetime.datetime.now()
+                trade['trade'] = log_data
+                trade['date'] = current_datetime
+                crypto_trade_details(trade)
         else:
             logging.info(f"Price not available or not within the desired range for {symbol}")
     except Exception as e:
