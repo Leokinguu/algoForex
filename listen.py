f80447c7dbd2006d1919599cd0bc5dc194efc8cf
Praveen <114330683+Leokinguu@users.noreply.github.com>
2024-01-16 09:23:13 +0530

    Commented MMFX

--- listen.py
+++ listen.py
@@ -10,19 +10,35 @@ client = TelegramClient('session_read', api_id, api_hash)
 
 print('Listening...')
 
-@client.on(events.NewMessage(chats=-1001447871772))
+
+@client.on(events.NewMessage(chats=-1001464335892))
 async def my_event_handler(event):
     statement = event.raw_text
-    if check_statement_forex(statement):
-        print('new from MMFX')
-        data = capture_trade_info(statement)
-        symbol = data['symbol']
+    if check_statement_gold(statement):
+        print('Signal from XAUUSD')
+        data = capture_trade_xauusd(statement)
+        symbol = 'XAUUSD'
         price = data['price']
         sl = data['stop_loss']
         direction = data['direction']
-        target = data['take_profit_1']
-        print(symbol, target)
-        await place_order(symbol, direction, price, target, sl, None,None, False)
+        tp1 = data['take_profit_1']
+        tp2 = data['take_profit_2']
+        tp3 = data['take_profit_3']
+        await place_order(symbol, direction, price, tp1, sl, tp2, tp3, True)
+
+# @client.on(events.NewMessage(chats=-1001447871772))
+# async def my_event_handler(event):
+#     statement = event.raw_text
+#     if check_statement_forex(statement):
+#         print('new from MMFX')
+#         data = capture_trade_info(statement)
+#         symbol = data['symbol']
+#         price = data['price']
+#         sl = data['stop_loss']
+#         direction = data['direction']
+#         target = data['take_profit_1']
+#         print(symbol, target)
+#         await place_order(symbol, direction, price, target, sl, None,None, False)
 
 @client.on(events.NewMessage(chats=-4026560717))
 async def my_event_handler(event):
@@ -74,21 +90,6 @@ async def my_event_handler(event):
         sl = data['stop_loss']
         print(tp1, sl)
         await place_orders_crypto(coin, price1, price2, tp1, tp2, sl)
-
-@client.on(events.NewMessage(chats=-1001464335892))
-async def my_event_handler(event):
-    statement = event.raw_text
-    if check_statement_gold(statement):
-        print('Signal from XAUUSD')
-        data = capture_trade_xauusd(statement)
-        symbol = 'XAUUSD'
-        price = data['price']
-        sl = data['stop_loss']
-        direction = data['direction']
-        tp1 = data['take_profit_1']
-        tp2 = data['take_profit_2']
-        tp3 = data['take_profit_3']
-        await place_order(symbol, direction, price, tp1, sl, tp2, tp3, True)
 
 
 client.start()
